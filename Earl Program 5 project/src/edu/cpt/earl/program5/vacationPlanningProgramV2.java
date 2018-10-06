53cc41b2250cae449f8af113b5633673a4fd0dd1
Acearl <42820499+Acearl@users.noreply.github.com>
2018-10-05 21:38:32 -0400

    Add files via upload

--- Earl Program 5 project/src/edu/cpt/earl/program5/vacationPlanningProgramV2.java
+++ Earl Program 5 project/src/edu/cpt/earl/program5/vacationPlanningProgramV2.java
@@ -1,7 +1,7 @@
 package edu.cpt.earl.program5;
 import java.util.Scanner;
 
-/** Vacation calculator: Assignment5
+/** Vacation calculator: Program5
  * Author: Andrew Earl
  * Course: CPT 167
  * Purpose: Calculate cost for travel destination and upgrades on travel
@@ -41,34 +41,55 @@ public class vacationPlanningProgramV2 {
 		char transportationSlection = ' ';
 		char upgradeSelection = ' ';
 
+		//The Cost of any Destination is $500 dollars, which is the DEFAULT_LODGING_FEE
+		
 		String userName = "";
 		String destinationName = "";
-		double destinationCost = 0.0;
 		String transportationName = "";
 		double transportationCost = 0.0;
 		String upgradeName = "";
 		double upgradeCost = 0.0;
+		double totalTripCost = 0.0;
+		double completeTripCost = 0.0;
 		
+		int counterDays = 0;
 		
+		//Start of method programs
 		welcomeBanner();
 		
 		userName = askUserName(input);
 		
-		destinationSlection = validateMainMenuSelection();
+		destinationSlection = validateMainMenuSelection(input, DESTINATION_ROME_ITALY, DESTINATION_LONDON_ENGLAND, DESTINATION_KEY_WEST_FLORDIA, DEFAULT_LODGING_FEE);
 		
-		transportationSlection = validateTransportationMenuSelection();
-		
-		upgradeSelection = validateUpgradeMenuSelection();
+		while (destinationSlection != 'q') {
+			
+			transportationSlection = validateTransportationMenuSelection(input, TRANSPORT_AIRPLANE, TRANSPORT_SHIP, AIRPLANE_COST, SHIP_COST);
+			
+			upgradeSelection = validateUpgradeMenuSelection(input, FIRST_CLASS_ACCOM, FIRST_CLASS_LODGING, GUIDED_TOUR, ALL_UPGRADED_SERVICES, NO_UPGRADED_SERVICES, FIRST_CLASS_ACCOM_COST, FIRST_CLASS_LODGING_COST, GUIDED_TOUR_COST, ALL_UPGRADED_SERVICES_COST, NO_UPGRADED_SERVICES_COST);
+					
+			destinationName = destinationDecider(destinationSlection, DESTINATION_ROME_ITALY, DESTINATION_LONDON_ENGLAND, DESTINATION_KEY_WEST_FLORDIA);
+			
+			transportationName = transportationDecider(transportationSlection, TRANSPORT_AIRPLANE, TRANSPORT_SHIP);
+			
+			transportationCost = transportationCostDecider(transportationSlection, AIRPLANE_COST, SHIP_COST);
+			
+			upgradeName = upgradeDecider(upgradeSelection, FIRST_CLASS_ACCOM, FIRST_CLASS_LODGING, GUIDED_TOUR, ALL_UPGRADED_SERVICES, NO_UPGRADED_SERVICES);
+			
+			upgradeCost = upgradeCostDecider(upgradeSelection, FIRST_CLASS_ACCOM_COST, FIRST_CLASS_LODGING_COST, GUIDED_TOUR_COST, ALL_UPGRADED_SERVICES_COST, NO_UPGRADED_SERVICES_COST);
+			
+			totalTripCost = totalTripCostCalculation(DEFAULT_LODGING_FEE, transportationCost, upgradeCost);
+			
+			totalTripCostReceipt(userName, destinationName, transportationName, upgradeName, transportationCost, DEFAULT_LODGING_FEE, upgradeCost, totalTripCost);
 				
-		destinationName = destinationDecider();
+			completeTripCost = completeTripCost + totalTripCost;
+			
+			counterDays++;
+			
+			destinationSlection = validateMainMenuSelection(input, DESTINATION_ROME_ITALY, DESTINATION_LONDON_ENGLAND, DESTINATION_KEY_WEST_FLORDIA, DEFAULT_LODGING_FEE);
 		
-		transportationName = transportationDecider();
+		}//end of main while loop
 		
-		transportationName = transportationCostDecider();
-		
-		upgradeName = upgradeDecider();
-		
-		upgradeCost = upgradeCostDecider();
+		farewellBanner(counterDays, completeTripCost);
 		
 	}//end of main
 	
@@ -82,8 +103,9 @@ public class vacationPlanningProgramV2 {
 		
 	}//end of welcomeBanner
 	
-	
 	public static String askUserName(Scanner borrowedInput) {
+		
+		String userName = "";
 		
 		System.out.println("To begin, What is your name? ");
 		
@@ -96,7 +118,7 @@ public class vacationPlanningProgramV2 {
 		
 	}//end askUserName
 	
-	public static char validateMainMenuSelection(Scanner borrowedInput) {
+	public static char validateMainMenuSelection(Scanner borrowedInput, String DESTINATION_ROME_ITALY, String DESTINATION_LONDON_ENGLAND, String DESTINATION_KEY_WEST_FLORDIA, double DEFAULT_LODGING_FEE) {
 	
 		char localMenuAnswer = ' ';
 		
@@ -105,8 +127,10 @@ public class vacationPlanningProgramV2 {
 		System.out.printf("%-3s%30s%3s%6.2f","\n|A|", DESTINATION_ROME_ITALY,"  $",DEFAULT_LODGING_FEE);
 		System.out.printf("%-3s%30s%3s%6.2f","\n|B|", DESTINATION_LONDON_ENGLAND,"  $",DEFAULT_LODGING_FEE);
 		System.out.printf("%-3s%30s%3s%6.2f","\n|C|", DESTINATION_KEY_WEST_FLORDIA,"  $",DEFAULT_LODGING_FEE);
-		
-		System.out.println("");
+		System.out.printf("%-3s%30s%3s","\n|Q|", " ","Quit");
+
+		System.out.println("\n\n(Type 'Q' to exit the program and/or get a final report)");
+
 		System.out.println("");
 		System.out.print(": ");
 		
@@ -114,7 +138,7 @@ public class vacationPlanningProgramV2 {
 		
 		localMenuAnswer = Character.toLowerCase(localMenuAnswer);
 		
-		while (localMenuAnswer != 'a' && localMenuAnswer != 'b' && localMenuAnswer != 'c') {
+		while (localMenuAnswer != 'a' && localMenuAnswer != 'b' && localMenuAnswer != 'c' && localMenuAnswer != 'q') {
 			
 			System.out.println("------------------------------------------------------------\n");
 			System.out.println("That input is invalid. Try again.\n");
@@ -136,7 +160,7 @@ public class vacationPlanningProgramV2 {
 	
 	}//end of validateMainMenuSelection
 	
-	public static char validateTransportationMenuSelection(Scanner borrowedInput) {
+	public static char validateTransportationMenuSelection(Scanner borrowedInput, String TRANSPORT_AIRPLANE, String TRANSPORT_SHIP, double AIRPLANE_COST, double SHIP_COST) {
 		
 		char localMenuAnswer = ' ';
 		
@@ -172,9 +196,11 @@ public class vacationPlanningProgramV2 {
 			
 		}//end of validateTransportationMenuSelection while loop
 		
+		return localMenuAnswer;
+		
 	}//end of validateTransportationMenuSelection
 	
-	public static char validateUpgradeMenuSelection() {
+	public static char validateUpgradeMenuSelection(Scanner borrowedInput, String FIRST_CLASS_ACCOM, String FIRST_CLASS_LODGING, String GUIDED_TOUR, String ALL_UPGRADED_SERVICES, String NO_UPGRADED_SERVICES, double FIRST_CLASS_ACCOM_COST, double FIRST_CLASS_LODGING_COST, double GUIDED_TOUR_COST, double ALL_UPGRADED_SERVICES_COST, double NO_UPGRADED_SERVICES_COST) {
 		
 		char localMenuAnswer = ' ';
 		
@@ -190,7 +216,7 @@ public class vacationPlanningProgramV2 {
 		System.out.println("");
 		System.out.print(": ");
 		
-		localMenuAnswer = input.nextLine().charAt(0);
+		localMenuAnswer = borrowedInput.nextLine().charAt(0);
 		
 		localMenuAnswer = Character.toLowerCase(localMenuAnswer);
 		
@@ -209,15 +235,19 @@ public class vacationPlanningProgramV2 {
 			System.out.println("");
 			System.out.print(": ");
 			
-			localMenuAnswer = input.nextLine().charAt(0);
+			localMenuAnswer = borrowedInput.nextLine().charAt(0);
 			
 			localMenuAnswer = Character.toLowerCase(localMenuAnswer);
 			
 		}//end of validateUpdateMenuSelection while loop
 		
+		return localMenuAnswer;
+		
 	}//end of validateUpdateMenuSelection
 	
-	public static String destinationDecider() {
+	public static String destinationDecider(char localMenuAnswer, String DESTINATION_ROME_ITALY, String DESTINATION_LONDON_ENGLAND, String DESTINATION_KEY_WEST_FLORDIA) {
+		
+		String destination = "";
 		
 		if (localMenuAnswer == 'a') {
 			destination = DESTINATION_ROME_ITALY;
@@ -229,12 +259,14 @@ public class vacationPlanningProgramV2 {
 			destination = DESTINATION_KEY_WEST_FLORDIA;
 		}
 		
-		return destination
+		return destination;
 				
 	}//end of destinationDecider
 	
-	public static String transportationDecider() {
+	public static String transportationDecider(char localMenuAnswer, String TRANSPORT_AIRPLANE, String TRANSPORT_SHIP) {
 			
+		String modeOfTransport = "";
+		
 		if (localMenuAnswer == 'a') {
 			
 			modeOfTransport = TRANSPORT_AIRPLANE;
@@ -250,7 +282,9 @@ public class vacationPlanningProgramV2 {
 		
 	}//end of transportationDecider
 	
-	public static String transportationCostDecider(transportationMenuAnswer) {
+	public static double transportationCostDecider(char localMenuAnswer, double AIRPLANE_COST, double SHIP_COST) {
+		
+		double modeOfTransportCost = 0.0;
 		
 		if (localMenuAnswer == 'a') {
 			
@@ -261,11 +295,13 @@ public class vacationPlanningProgramV2 {
 			modeOfTransportCost = SHIP_COST;
 		}
 		
-		return modeOfTransCost;
+		return modeOfTransportCost;
 		
 	}//end of transportationCostDecider
 	
-	public static String upgradeDecider() {
+	public static String upgradeDecider(char localMenuAnswer, String FIRST_CLASS_ACCOM, String FIRST_CLASS_LODGING, String GUIDED_TOUR, String ALL_UPGRADED_SERVICES, String NO_UPGRADED_SERVICES) {
+		
+		String upgradeName = "";
 		
 		if (localMenuAnswer == 'a') {
 			
@@ -288,9 +324,13 @@ public class vacationPlanningProgramV2 {
 			upgradeName = NO_UPGRADED_SERVICES;
 		}
 		
+		return upgradeName;
+		
 	}//end of upgradeDecider
-	
-	public static String upgradeCostDecider() {
+
+	public static double upgradeCostDecider(char localMenuAnswer, double FIRST_CLASS_ACCOM_COST, double FIRST_CLASS_LODGING_COST, double GUIDED_TOUR_COST, double ALL_UPGRADED_SERVICES_COST, double NO_UPGRADED_SERVICES_COST) {
+		
+		double upgradeValue;
 		
 		if (localMenuAnswer == 'a') {
 			
@@ -318,6 +358,61 @@ public class vacationPlanningProgramV2 {
 			
 		}
 		
+		return upgradeValue;
+		
 	}//end of upgradeCostDecider
+
+	public static double totalTripCostCalculation(double DEFAULT_LODGING_FEE, double transportationCost, double upgradeCost) {
+		
+		double totalCost = 0.0;
+		
+		totalCost = DEFAULT_LODGING_FEE + transportationCost + upgradeCost;
+		
+		return totalCost;
+	}
+	
+	public static void totalTripCostReceipt(String userName, String destinationName, String transportationName, String upgradeName, double DEFAULT_LODGING_FEE, double upgradeCost, double transportationCost, double totalTripCost) {
+		
+		//Display output
+		
+		System.out.println("------------------------------------------------------------");
+		System.out.println("-----------------------Results-Menu-------------------------");
+		System.out.println("------------------------------------------------------------\n");
+		
+		System.out.println("The information on " + userName + "'s Vacation.\n");
+		
+		System.out.printf("%-20s%20s%3s%10s","\nLabel", "Name","  $", "Dollars");
+		System.out.println("\n************************************************************");
+		System.out.printf("%-20s%20s%3s%8.2f","\nDestination", destinationName,"  $", DEFAULT_LODGING_FEE);
+		System.out.printf("%-20s%20s%3s%8.2f","\nTransportation", transportationName,"  $", transportationCost);
+		System.out.printf("%-20s%20s%3s%8.2f","\nName of Upgrade(s)",upgradeName,"  $", upgradeCost);
+		System.out.println("\n************************************************************");
+		System.out.printf("%-20s%3s%6.2f","\n\tTotal Trip Cost","  $", totalTripCost);
+		
+		System.out.println("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+		System.out.println("Thank you for your time and have fun on your trip.");
+		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+				
+				
+				
+	}
+
+	public static void farewellBanner(int counterDays, double completeTripCost) {
+		
+		System.out.println("------------------------------------------------------------\n");
+
+		if(completeTripCost > 0.0) {
+			
+			System.out.println("Your trip is " + counterDays + " day(s)\n");
+			System.out.printf("%-20s%6.2f%2s","Your trip costs $", completeTripCost, "\n");
+			
+		}
+		System.out.println("Thank you for your time and we hope to see you again.");
+		
+		System.out.println("------------------------------------------------------------\n");
+		
+		
+
+	}
 
 }//End of Main
